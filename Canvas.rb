6355cb5c0102a0852287ec7afaf94772ab5259e4
Ash-the-practical-programmer <83904240+Ash-the-practical-programmer@users.noreply.github.com>
2022-01-14 10:03:12 +0530

    Update Canvas.rb
    #Updating Canvas class making the "canvas in console" more compatible .

--- Canvas.rb
+++ Canvas.rb
@@ -1,35 +1,37 @@
 class Canvas
     def initialize(text="Hi, I'm Canvas in Console",minho=43)
         puts "#Prsenting Canvas in Console in Ruby"
-        p = lambda { puts "–"+"—"*minho+"–" }
+        p = lambda{ puts "–"+"—"*minho+"–" }
         p.call 
         len = text.length
-        space_1 = (minho-len)/2
-        space_2 = if len%2 == 1
-            space_1 
-        else 
-            space_1+1
-        end
+        space_1,space_2 = 0,0
+        temp = text.scan(/.{0,#{minho-1}}[a-z.!?,;](?:\b|$)/mi)
         20.times {|time|
-        unless time == 9
-            p.call 
+        unless time == (20 - temp.length)/2
+            puts "–" +" "*minho+"–"
         else
-            puts ("–"+" "*space_1+text+" "*space_1+"–").center(minho+2)
+            for i in temp
+                if i.length % 2 == 0
+                    space_1 = (minho-i.length)/2
+                    space_2 = 43 - i.length - space_1
+                else
+                    space_1 = (minho-i.length)/2
+                    space_2 = space_1
+                end
+                puts "–"+(" "*space_1+i+" "*space_2).center(minho)+"–"
+            end
         end
         }
         p.call 
         if (text.length % 2 == 0) && (text.length > minho)
-            puts "\n#Warning: Your inputted text's length excceeded 43 and it was also even"
+            puts "\n#Bot: Your inputted text's length excceeded 43 and it was also even"
         elsif (text.length == 0)
-            puts "\n#Warning: Your inputted text's length is 0"
-        elsif (text.length > minho)
-            puts "\n#Warning: Your inputted text's length exceeded #{minho}"
-        elsif (text.length % 2 == 0)
-            puts "\n#Warning: Your inputted text's length is even"
-    
+            puts "\n#Bot: Your inputted text's length is 0"
         end
-        puts "\n#Note: Current version of Canvas in Console \n       is only compatible with text/string of \n       odd length not exceeding 45"
+        puts "\n#Note: Current version of Canvas in Console \n       is now compatible with text/string of \n       any length but only for strings made\n       of alphabets"
         puts "\n#Update: The project is still in progress /\n         under construction and new features \n         will be added very soon.\n"
         puts "\n#Mission: No more any console will be boring.\n          It can definitely be more fun, \n          interesting and better than any \n          graphical interface. We commit it."
     end
 end
+
+#Import this file , create an instance of Canvas and make use of its helpful methods .
