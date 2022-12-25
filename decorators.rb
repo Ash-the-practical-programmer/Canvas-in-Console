98618698f6bccd1a9177e71994aee9a9de3ec3d4
Ash <83904240+Ash-the-practical-programmer@users.noreply.github.com>
2022-12-25 09:53:56 +0530

    Update decorators.rb

--- decorators.rb
+++ decorators.rb
@@ -1,4 +1,4 @@
-Decorators = [
+$Decorators = [
 ->(s){puts "+"+"—+"*(s.size/2)+"\n"+s+"\n+"+"—+"*(s.size/2)},
 ->(s){puts "* "*(s.size/2+1)+"\n"+s+"\n"+"* "*(s.size/2+1)},
 ->(s){puts "*"*s.size+"\n"+s+"\n"+"*"*s.size},
