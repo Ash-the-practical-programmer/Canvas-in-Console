7ea36514a89051166bc169ecec0cb900572f11e2
Ash <83904240+Ash-the-practical-programmer@users.noreply.github.com>
2022-12-25 08:55:09 +0530

    Update Canvas.rb

--- Canvas.rb
+++ Canvas.rb
@@ -1,42 +1,42 @@
 class Canvas
-    attr_accessor :text, :minho
-    def initialize(text="Hi, I'm Canvas in Console",minho=43)
-        @text = text
-        @minho = minho
-        puts "#Prsenting Canvas in Console in Ruby"
-        p = lambda{ puts "–"+"—"*minho+"–" }
-        p.call 
-        len = text.length
-        space_1,space_2 = 0,0
-        temp = text.scan(/.{0,#{minho-1}}[a-z.!?,;](?:\b|$)/mi)
-        20.times {|time|
-        unless time == (20 - temp.length)/2
-            puts "–" +" "*minho+"–"
-        else
-            for i in temp
-                if i.length % 2 == 0
-                    space_1 = (minho-i.length)/2
-                    space_2 = 43 - i.length - space_1
-                else
-                    space_1 = (minho-i.length)/2
-                    space_2 = space_1
-                end
-                puts "–"+(" "*space_1+i+" "*space_2).center(minho)+"–"
-            end
+  attr_accessor :text, :minho
+  def initialize(text="Hi, I'm Canvas in Console",minho=43)
+    @text = text
+    @minho = minho
+    puts "#Prsenting Canvas in Console in Ruby"
+    p = lambda{ puts "–"+"—"*minho+"–" }
+    p.call 
+    len = text.length
+    space_1,space_2 = 0,0
+    temp = text.scan(/.{0,#{minho-1}}[a-z.!?,;](?:\b|$)/mi)
+    20.times {
+      unless _1 == (20 - temp.length)/2
+        puts "–" +" "*minho+"–"
+      else
+        for i in temp
+          if i.size.even?
+            space_1 = (minho-i.length)/2
+            space_2 = 43 - i.length - space_1
+          else
+            space_1 = (minho-i.length)/2
+            space_2 = space_1
+          end
+          puts "–"+(" "*space_1+i+" "*space_2).center(minho)+"–"
         end
-        }
-        p.call 
+      end
+    }
+    p.call 
+  end
+  def describe
+    if @text.size.even? && @text.length > minho
+      puts "\n#Bot: Your inputted text's length excceeded #{minho} and it was also even"
+    elsif @text.size.zero?
+      puts "\n#Bot: Your inputted text's length is 0"
     end
-    def describe
-        if (@text.length % 2 == 0) && (@text.length > minho)
-            puts "\n#Bot: Your inputted text's length excceeded #{minho} and it was also even"
-        elsif (@text.length == 0)
-            puts "\n#Bot: Your inputted text's length is 0"
-        end
-        puts "\n#Note: Current version of Canvas in Console \n       is now compatible with text/string of \n       any length but only for strings made\n       of alphabets"
-        puts "\n#Update: The project is still in progress /\n         under construction and new features \n         will be added very soon.\n"
-        puts "\n#Mission: No more any console will be boring.\n          It can definitely be more fun, \n          interesting and better than any \n          graphical interface. We commit it."
-    end
+    puts "\n#Note: Current version of Canvas in Console \n       is now compatible with text/string of \n       any length but only for strings made\n       of alphabets"
+    puts "\n#Update: The project is still in progress /\n         under construction and new features \n         will be added very soon.\n"
+    puts "\n#Mission: No more any console will be boring.\n          It can definitely be more fun, \n          interesting and better than any \n          graphical interface. We commit it."
+  end
 end
 
 #Import this file , create an instance of Canvas and make use of its helpful methods .
