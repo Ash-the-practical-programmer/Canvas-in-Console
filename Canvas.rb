afb324c275743d9f1689c79164e4ffc4bb7ea923
Ash <83904240+Ash-the-practical-programmer@users.noreply.github.com>
2022-12-25 16:51:12 +0530

    Update Canvas.rb

--- Canvas.rb
+++ Canvas.rb
@@ -1,16 +1,17 @@
 require_relative "/decorators.rb"
 class Canvas
   attr_accessor :text, :minho
-  def initialize(text="Hi, I'm Canvas in Console",minho=43)
+  def initialize text="Hi, I'm Canvas in Console",minho=43
     @text = text
     @minho = minho
     $Decorators.sample.call "#Presenting Canvas in Console in Ruby"
     p = lambda{ puts "–"+"—"*minho+"–" }
     p.call 
-    space_1,space_2 = 0,0
+    space_1 = ((minho-text.size)/2).abs
+    space_2 = space_1 + (text.size.odd? ? 0 : 1)
     temp = text.scan(/.{0,#{minho-1}}[a-z.!?,;](?:\b|$)/mi)
-    20.times {
-      unless _1 == (20 - temp.size)/2
+    20.times {|time|
+      unless time == (20 - temp.size)/2
         puts "–" +" "*minho+"–"
       else
         for i in temp
@@ -28,14 +29,23 @@ class Canvas
     p.call 
   end
   def describe
-    if @text.size.even? && @text.length > minho
-      puts "\n#Bot: Your inputted text's length excceeded #{minho} and it was also even"
+    if @text.size.even? && @text.size > minho
+      puts "\n#Bot: Your inputted text's length excceeded 43 and it was also even"
     elsif @text.size.zero?
       puts "\n#Bot: Your inputted text's length is 0"
     end
-    puts "\n#Note: Current version of Canvas in Console \n       is now compatible with text/string of \n       any length but only for strings made\n       of alphabets"
-    puts "\n#Update: The project is still in progress /\n         under construction and new features \n         will be added very soon.\n"
-    puts "\n#Mission: No more any console will be boring.\n          It can definitely be more fun, \n          interesting and better than any \n          graphical interface. We commit it."
+    print "\n#Note: "
+    put ="Current version of Canvas in Console is now compatible with text/string of any length but only for strings made of alphabets".scan(/.{0,37}[a-z.!?,;](?:\b|$)/mi)
+    puts put[0]
+    (put.size-1).times { puts " "*6+put[_1+1] }
+    print "\n#Update: "
+    put_2 = "The project is still in progress under construction and new features will be added very soon.\n".scan(/.{0,35}[a-z.!?,;](?:\b|$)/mi)
+    puts put_2[0]
+    (put_2.size-1).times { puts " "*8+put_2[_1+1] }
+    print "\n#Mission: "
+    put_3 = "No more any console will be boring.It can definitely be more fun,interesting and better than any graphical interface.".scan(/.{0,34}[a-z.!?,;](?:\b|$)/mi)
+    puts put_3[0]
+    (put_3.size-1).times { puts " "*10+put_3[_1+1] }
   end
 end
 
